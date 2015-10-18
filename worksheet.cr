4b0f88fde84045ff514fff7a13d53ce706315b93
Benoist Claassen <benoist.claassen@gmail.com>
2015-10-18 15:09:44 +0200

    added worksheet

--- worksheet.cr
+++ worksheet.cr
@@ -1,22 +1,79 @@
 require "./src/lib/lib_ssl"
 # require "secure_random"
 
-NULL = Pointer(Void).null
-# initialize
-cipher = LibCrypto.evp_get_cipherbyname "aes-128-cbc"
-ctx = LibCrypto.evp_cipher_ctx_new
+LibSSL.ssl_library_init
+LibSSL.openssl_add_all_algorithms
+LibSSL.err_load_crypto_strings
+LibSSL.ssl_load_error_strings
 
-# init
+class Cipher
+  class Error < Exception
+  end
 
-key = "\0" * 16
-iv = "\0" * 16
-LibCrypto.evp_cipherinit_ex(ctx, cipher, NULL, key, iv, 1)
+  def initialize(name = "aes-128-cbc")
+    cipher = LibCrypto.evp_get_cipherbyname name
+    raise ArgumentError.new "unsupported cipher algorithm #{name.inspect}" unless cipher
 
-# key_len = LibCrypto.evp_cipher_key_length cipher
-# iv_len = LibCrypto.evp_cipher_iv_length cipher
+    @ctx = LibCrypto.evp_cipher_ctx_new
 
-puts cipher
-puts ctx
+    cipherinit(cipher: cipher, key: "\0" * 16, iv: "\0" * 16, enc: 1)
+  end
 
-# puts key_len.inspect
-# puts iv_len.inspect
+  def update(data)
+    ina = data.to_slice
+
+    outa = Slice(UInt8).new(block_size)
+
+    if LibCrypto.evp_cipherupdate(@ctx, outa.to_unsafe, out outl, ina.pointer(0), ina.size) != 1
+      raise Error.new "EVP_CipherUpdate"
+    end
+    puts outl
+
+    outa
+  end
+
+  def final
+    outa = Slice(UInt8).new(block_size)
+
+    if LibCrypto.evp_cipherfinal_ex(@ctx, outa.to_unsafe, out outl) != 1
+      raise Error.new "EVP_CipherFinal_ex"
+    end
+
+    outa
+  end
+
+  def block_size
+    LibCrypto.evp_cipher_block_size cipher
+  end
+
+  def key_len
+    LibCrypto.evp_cipher_key_length cipher
+  end
+
+  def iv_len
+    LibCrypto.evp_cipher_iv_length cipher
+  end
+
+  NULL = Pointer(Void).null
+
+  private def cipherinit cipher = NULL, engine = NULL, key = Pointer(UInt8).null, iv = Pointer(UInt8).null, enc = -1
+    if LibCrypto.evp_cipherinit_ex(@ctx, cipher, engine, key, iv, enc) != 1
+      raise Error.new "EVP_CipherInit_ex"
+    end
+
+    nil
+  end
+
+  private def cipher
+    LibCrypto.evp_cipher_ctx_cipher @ctx
+  end
+end
+
+cipher = Cipher.new
+
+output = MemoryIO.new
+
+puts File.read("./spec/cipher_spec.ciphertext").bytes
+output.write(cipher.update("DATA" * 5))
+output.write(cipher.final)
+puts output.to_slice
